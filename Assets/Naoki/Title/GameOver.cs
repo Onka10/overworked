5450af65243d82652666adef8035a40606a262cd
kenzyakato <87981484+kenzyakato@users.noreply.github.com>
2022-05-07 23:09:33 +0900

    Score表示

--- Assets/Naoki/Title/GameOver.cs
+++ Assets/Naoki/Title/GameOver.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameOver : MonoBehaviour
 {
-    int Score;
+    public int Score;
     public GameObject OverScene;
+    public Text t1;
+    public Text t2;
+    public Text t3;
+    public Text t4;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +19,15 @@ public class GameOver : MonoBehaviour
     public void PlayOverAnime()
     {
         OverScene.SetActive(true);
-        var OverAnimator = OverScene.GetComponent<Animator>();
+        Animator OverAnimator = OverScene.GetComponent<Animator>();
         OverAnimator.SetInteger("Score", Score);
+        string s = Score.ToString();
+
+        t1.text = s[0].ToString(); // 1
+        t2.text = s[1].ToString(); // 1
+        t3.text = s[2].ToString(); // 1
+        t4.text = s[3].ToString(); // 1
+
 
     }
     public void ResetOverAnime()
