7631314e51e3733093f3ca29c4133445557867a3
yanoharuto <8bitharuto@gmail.com>
2022-05-08 18:21:09 +0900

    SEを鳴らせるやつ

---
+++ Assets/Sound/C#Script/Yano/SEManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SEManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource mPlayerAttackAudioSource;
+    [SerializeField] private AudioSource mClickAudioSource;
+    /// <summary>
+    /// 攻撃したときの音
+    /// </summary>
+    public void StartAttackSE()
+    {
+
+        mPlayerAttackAudioSource.Play();
+
+    }
+    /// <summary>
+    /// クリックしたときの音
+    /// </summary>
+    public void StartClickSE()
+    {
+
+        mClickAudioSource.Play();
+
+    }
+}
